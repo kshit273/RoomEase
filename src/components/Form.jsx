3ce0273c7ab84a13ef17355dad953c615da2e4e5
Kshitij <kshitijsharma273@gmail.com>
2025-06-09 16:51:00 +0530

    created the responsiveness of the contact section

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,37 +1,71 @@
+import { useMediaQuery } from "react-responsive";
+
 const Form = () => {
+  const isSmallScreen = useMediaQuery({ minWidth: 851, maxWidth: 1400 });
+  const isMobile = useMediaQuery({ maxWidth: 850 });
   return (
-    <div className="flex-1 h-full flex flex-col justify-center mx-[50px] mt-[50px]">
+    <div
+      className={`flex-1 h-full flex flex-col justify-center ${
+        isMobile ? `mx-[20px] mt-[30px]` : `mx-[50px] mt-[50px]`
+      }  `}
+    >
       <form
         action="submit"
         className="w-full h-full flex flex-col justify-center"
       >
-        <div className="h-full w-full flex flex-col gap-[30px]">
-          <div className="flex gap-[50px]">
+        <div
+          className={`h-full w-full flex flex-col ${
+            isMobile ? `gap-[10px]` : `gap-[30px]`
+          } `}
+        >
+          <div className={`flex ${isMobile ? `gap-[10px]` : `gap-[50px]`}`}>
             <input
               type="text"
-              className="flex-1 w-full border-[2px] border-[#707070] rounded-[10px] h-[70px] text-[#d9d9d9] font-medium text-[20px] pl-[15px] placeholder-[#d9d9d9] placeholder:font-medium placeholder:text-[20px] placeholder:pl-[15px] bg-transparent"
+              className={`${
+                isMobile
+                  ? `h-[40px] text-[12px] pl-[8px] placeholder:text-[12px] placeholder:pl-[8px]`
+                  : ` h-[70px] text-[20px] pl-[15px] placeholder:text-[20px] placeholder:pl-[15px]`
+              } flex-1 w-full border-[2px] border-[#707070] rounded-[10px] text-[#d9d9d9] font-medium  placeholder-[#d9d9d9] placeholder:font-medium  bg-transparent`}
               placeholder="First name"
             />
             <input
               type="text"
-              className="flex-1 w-full border-[2px] border-[#707070] rounded-[10px] h-[70px] text-[#d9d9d9] font-medium text-[20px] pl-[15px] placeholder-[#d9d9d9] placeholder:font-medium placeholder:text-[20px] placeholder:pl-[15px] bg-transparent"
+              className={`${
+                isMobile
+                  ? `h-[40px] text-[12px] pl-[8px] placeholder:text-[12px] placeholder:pl-[8px]`
+                  : `h-[70px] text-[20px] pl-[15px] placeholder:text-[20px] placeholder:pl-[15px]`
+              } flex-1 w-full border-[2px] border-[#707070] rounded-[10px]  text-[#d9d9d9] font-medium  placeholder-[#d9d9d9] placeholder:font-medium  bg-transparent`}
               placeholder="Last name"
             />
           </div>
           <div>
             <input
               type="text"
-              className="w-full border-[2px] border-[#707070] rounded-[10px] h-[70px] text-[#d9d9d9] font-medium text-[20px] pl-[15px] placeholder-[#d9d9d9] placeholder:font-medium placeholder:text-[20px] placeholder:pl-[15px] bg-transparent"
+              className={`${
+                isMobile
+                  ? `h-[40px] text-[12px] pl-[8px] placeholder:text-[12px] placeholder:pl-[8px]`
+                  : `h-[70px] text-[20px] pl-[15px] placeholder:text-[20px] placeholder:pl-[15px]`
+              } w-full border-[2px] border-[#707070] rounded-[10px]  text-[#d9d9d9] font-medium placeholder-[#d9d9d9] placeholder:font-medium  bg-transparent`}
               placeholder="Your email"
             />
           </div>
           <div>
             <textarea
-              className="w-full border-[2px] border-[#707070] rounded-[10px] h-[490px] text-[#d9d9d9] font-medium text-[20px] pl-[25px] pt-[25px] placeholder-[#d9d9d9] placeholder:font-medium placeholder:text-[20px]  resize-none bg-transparent"
+              className={`${
+                isMobile
+                  ? `h-[250px] font-medium text-[12px] pl-[15px] pt-[15px] placeholder:text-[12px]`
+                  : `h-[490px] font-medium text-[20px] pl-[25px] pt-[25px] placeholder:text-[20px]`
+              } w-full border-[2px] border-[#707070] rounded-[10px]  text-[#d9d9d9]  placeholder-[#d9d9d9] placeholder:font-medium   resize-none bg-transparent`}
               placeholder="Your message"
             />
           </div>
-          <button className="bg-[#d72638] font-medium text-[#d9d9d9] text-[20px] rounded-[10px] h-[50px] w-[140px]">
+          <button
+            className={`${
+              isMobile
+                ? `text-[11px] h-[30px] w-[80px] rounded-[6px]`
+                : `text-[20px] h-[50px] w-[140px] rounded-[10px]`
+            } bg-[#d72638] font-medium text-[#d9d9d9]   `}
+          >
             Submit
           </button>
         </div>
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,25 +1,42 @@
-import React from "react";
 import Form from "../components/Form";
+import { useMediaQuery } from "react-responsive";
 
 const Contact = () => {
+  const isSmallScreen = useMediaQuery({ minWidth: 851, maxWidth: 1400 });
+  const isMobile = useMediaQuery({ maxWidth: 850 });
   return (
-    <section id="contact" className="relative overflow-hidden pt-[100px]">
-      <div className="bg-[#1a1a1a] h-[1000px] w-full flex">
+    <section
+      id="contact"
+      className={`relative overflow-hidden z-2 ${
+        isMobile ? `pt-[40px]` : `pt-[100px]`
+      } `}
+    >
+      <div
+        className={`bg-[#1a1a1a] ${
+          isMobile ? `h-[500px]` : `h-[1000px]`
+        }  w-full flex`}
+      >
         <div className="flex flex-col flex-1">
           <div className="flex flex-col items-center">
-            <div className="text-[#d9d9d9] font-medium text-[50px] mt-[50px]">
+            <div
+              className={`text-[#d9d9d9] font-medium ${
+                isMobile ? `text-[20px] mt-[20px]` : `text-[50px] mt-[50px]`
+              } `}
+            >
               Get in touch
             </div>
           </div>
           <Form />
         </div>
-        <div className="h-full flex items-end">
-          <img
-            src="./images/contactRoom.png"
-            alt=""
-            className="h-full w-auto object-contain"
-          />
-        </div>
+        {isSmallScreen || isMobile ? null : (
+          <div className="h-full flex items-end">
+            <img
+              src="./images/contactRoom.png"
+              alt=""
+              className="h-full w-auto object-contain"
+            />
+          </div>
+        )}
       </div>
     </section>
   );
